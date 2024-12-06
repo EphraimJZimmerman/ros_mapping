47ffc16831b06dfb37ded52abebbce84dfdf0dd9
Altakie <metradarksword@gmail.com>
2024-12-06 15:11:04 +0000

    trying to use just gyroscope

--- imu_converter.py
+++ imu_converter.py
@@ -2,6 +2,7 @@
 
 import rospy
 from sensor_msgs.msg import Imu
+from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
 
 
@@ -10,13 +11,33 @@ class ImuConverter():
         self.imu_sub = rospy.Subscriber(
             '/imu/data', Imu, callback=self.imu_cb)
 
+        self.imu_sub = rospy.Subscriber(
+            '/odom', Odometry, callback=self.odom_cb)
+
+        self.odom_yaw = 0
+        self.imu_yaw = 0
+
     def imu_cb(self, msg):
-        orientations = [msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w]
+        orientations = [msg.orientation.x, msg.orientation.y,
+                        msg.orientation.z, msg.orientation.w]
         (roll, pitch, yaw) = euler_from_quaternion(orientations)
-        print(yaw)
+        self.imu_yaw = yaw
+
+    def odom_cb(self, msg):
+        orientation = msg.pose.pose.orientation
+        orientations = [orientation.x, orientation.y,
+                        orientation.z, orientation.w]
+        (roll, pitch, yaw) = euler_from_quaternion(orientations)
+        self.odom_yaw = yaw
+
+    def run(self):
+        while not rospy.is_shutdown():
+            print(
+                f" Odom_yaw: {self.odom_yaw : .2f} ||| Imu yaw: {self.imu_yaw : .2f}")
 
 
 if __name__ == '__main__':
     rospy.init_node('imu_converter')
     imu_converter = ImuConverter()
+    imu_converter.run()
     rospy.spin()
---
+++ mag_reader.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import MagneticField
+import math
+
+
+def magnetic_callback(msg):
+    # Extract magnetic field components in ENU convention
+    Bx = msg.magnetic_field.x  # East component
+    By = msg.magnetic_field.y  # North component
+
+    # Calculate the yaw angle (heading) in radians
+    # yaw_radians = math.atan2(Bx, By)
+    yaw_radians = math.atan2(By, Bx)
+
+    # Convert radians to degrees and normalize to [0, 360)
+    yaw_degrees = (math.degrees(yaw_radians) + 360) % 360
+
+    # Determine direction based on yaw
+    if 45 <= yaw_degrees < 135:
+        direction = "East"
+    elif 135 <= yaw_degrees < 225:
+        direction = "South"
+    elif 225 <= yaw_degrees < 315:
+        direction = "West"
+    else:
+        direction = "North"
+
+    rospy.loginfo(f"Yaw: {yaw_degrees:.2f} degrees, Facing: {direction}")
+
+
+def magnetic_orientation():
+    rospy.init_node('magnetic_orientation', anonymous=True)
+    rospy.Subscriber("/imu/mag", MagneticField, magnetic_callback)
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    try:
+        magnetic_orientation()
+    except rospy.ROSInterruptException:
+        pass
